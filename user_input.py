af888e2941e4c69a5cf037ac5404453b9a2990ab
Gikubu <davidndungu8203@gmail.com>
2024-02-23 17:41:41 +0300

    hello this is my simple python project

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("what is your name?")
+age = input("what is your age?")
+location =input("where do you live?")
+print(f"Hello {name},you are {age} years old and live in {location}")
+
+
